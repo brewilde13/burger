00b68003844ed36d2003e19370ab35c4819aa20b
Brooke Wildermuth <wildermuthbrooke@gmail.com>
2018-03-06 18:19:07 -0600

    Initial Folder/File Setup and SQL Schema/Seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name) VALUES ('Peanut Butter Bacon Burger');
+INSERT INTO burger (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burger (burger_name) VALUES ('Double Cheeseburger');
